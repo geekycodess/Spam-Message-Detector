6898867eee4944ae235e735d39e093be24b3326c
arihant1805 <22cs3016@rgipt.ac.in>
2024-06-16 00:47:18 +0530

    updated main.py Dockerfile requirements.txt

--- Dockerfile
+++ Dockerfile
@@ -2,4 +2,4 @@ FROM python:3.11.9-slim
 COPY . ./app
 WORKDIR /app/app
 RUN pip install -r requirements.txt
-CMD [ "uvicorn", 'main:App']
+CMD [ "uvicorn", "main:App" ]
--- app/main.py
+++ app/main.py
@@ -1,13 +1,13 @@
 import pickle
-from tensorflow.keras.models import load_model
-from tensorflow.keras.preprocessing.sequence import pad_sequences
+import tensorflow
+from keras.utils import pad_sequences
 from fastapi import FastAPI 
 import numpy as np
 
 App = FastAPI()
 maxlen=189
 
-model=load_model('model.h5')
+model=tensorflow.keras.models.load_model('model.h5')
 tokenizer = pickle.load(open('Tokenizer.pkl', 'rb'))
 
 @App.post('/')
@@ -16,7 +16,7 @@ def spamDetector(msg: str):
     tokenized=tokenizer.texts_to_sequences(inp)
     padded = pad_sequences(tokenized, maxlen)
 
-    pred = model.predict(padded)
+    pred = model.predict(padded, verbose=0)
 
     if pred>=0.35: ## For safety concerns i droped the thrushold
         return {
